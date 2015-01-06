c71f81a50b76ee6c82d1f1ca7a7d8627ba0681f2
weswhitney <wesleywhitney@gmail.com>
2015-01-05 22:09:49 -0700

    added auth controller and the the css file to scss

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,2 +1,19 @@
 class AuthenticationsController < ApplicationController
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to_previous_url_or_projects
+    else
+      @sign_in_error = "Username / password combination is invalid"
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
 end
