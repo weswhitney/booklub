7111972585bced423019b270de22d7d794189fe8
weswhitney <wesleywhitney@gmail.com>
2015-01-08 10:44:33 -0700

    added signup and log in forms and tests

--- spec/signup_spec.rb
+++ spec/signup_spec.rb
@@ -1,17 +1,37 @@
 require "rails_helper"
 
-  feature "signup" do
+feature "signup" do
 
-    scenario "user can sign up" do
+  scenario "user can sign up" do
 
-      visit "/"
+    visit root_path
 
+    within(".signup-form") do
       fill_in("username", with: "Ross")
-      fill_in("email", with: "ross@friends.com", match: :first)
+      fill_in("email", with: "ross@friends.com")
       fill_in("password", with: "pivot")
       click_button("join")
-      save_and_open_page
-      expect(page).to have_content("Ross")
     end
 
+    expect(page).to have_content("Ross")
   end
+
+  scenario "users can log in" do
+
+    User.create!(
+      :username => "Ross",
+      :email => "ross@friends.com",
+      :password => "pivot"
+    )
+
+    visit "/"
+    within(".user-form") do
+      fill_in("email", with: "ross@friends.com")
+      fill_in("password", with: "pivot")
+      click_on("Login")
+    end
+
+    expect(page).to have_content("Ross")
+  end
+
+end
