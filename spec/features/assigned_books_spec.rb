b1e3aa0551ca4bd14b6b81d607c4542d8190140a
weswhitney <wesleywhitney@gmail.com>
2015-02-11 13:42:44 -0700

    heroku migrate

--- spec/features/assigned_books_spec.rb
+++ spec/features/assigned_books_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
+
 feature 'assigned_books' do
+
+ before do
+   create_bookclub = Bookclub.new
+ end
 
   scenario 'a user can delete a book from the bookclub' do
 
     create_assigned_book = AssignedBook.new
-    # visit bookclub_assigned_book_path(@bookclub, @assigned_book)
+    visit bookclub_assigned_book_path(@bookclub, @assigned_book)
   end
 
 end
--- spec/features/support/object_creation_methods.rb
+++ spec/features/support/object_creation_methods.rb
@@ -1,4 +1,20 @@
+def create_user
+  @user = User.create!(
+  email: 'user@example.com'
+  password: 'password'
+  username: 'uex'
+  )
+end
+
+def create_bookclub
+  @bookclub = BookClub.create!(
+  name: 'cool book lub'
+  )
+end
+
 def create_assigned_book
   @assigned_book = AssignedBook.create!(
+  bookclub_id: @bookclub.id,
+  user_id: user.id
   )
 end
